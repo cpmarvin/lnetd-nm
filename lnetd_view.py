8f2f2b2973183b765d1edf40bcba8169c641b0d1
cpmarvin <cpmarvin@gmail.com>
2020-11-09 16:20:06 +0000

    allow shift modifier for drag on view

--- lnetd_view.py
+++ lnetd_view.py
@@ -2,8 +2,29 @@ import math
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 
-from PyQt5.QtCore import Qt, QSize, QTimer, QPointF, QRectF, QMetaObject, QRect , QCoreApplication, QPoint, QLineF , pyqtSlot
-from PyQt5.QtGui import QPainter, QBrush, QPen, QFont, QIcon, QTransform, QPalette, QColor
+from PyQt5.QtCore import (
+    Qt,
+    QSize,
+    QTimer,
+    QPointF,
+    QRectF,
+    QMetaObject,
+    QRect,
+    QCoreApplication,
+    QPoint,
+    QLineF,
+    pyqtSlot,
+)
+from PyQt5.QtGui import (
+    QPainter,
+    QBrush,
+    QPen,
+    QFont,
+    QIcon,
+    QTransform,
+    QPalette,
+    QColor,
+)
 from PyQt5.QtWidgets import (
     QApplication,
     QWidget,
@@ -24,29 +45,29 @@ from PyQt5.QtWidgets import (
     QSpacerItem,
     QMainWindow,
     QMenuBar,
-    QOpenGLWidget
+    QOpenGLWidget,
 )
 
-class GraphicsView(QtWidgets.QGraphicsView):
 
+class GraphicsView(QtWidgets.QGraphicsView):
     def __init__(self, parent=None):
         super(GraphicsView, self).__init__(parent)
-        #see https://doc.qt.io/qt-5/qgraphicsview.html#renderHints-prop
-        self.setRenderHint( QtGui.QPainter.Antialiasing )
-        self.setRenderHint( QtGui.QPainter.TextAntialiasing )
-        self.setRenderHint( QtGui.QPainter.SmoothPixmapTransform )
+        # see https://doc.qt.io/qt-5/qgraphicsview.html#renderHints-prop
+        self.setRenderHint(QtGui.QPainter.Antialiasing)
+        self.setRenderHint(QtGui.QPainter.TextAntialiasing)
+        self.setRenderHint(QtGui.QPainter.SmoothPixmapTransform)
 
-        #drag options
-        #self.setDragMode(GraphicsView.ScrollHandDrag)
+        # drag options
+        # self.setDragMode(GraphicsView.ScrollHandDrag)
         self.setDragMode(GraphicsView.RubberBandDrag)
 
-        #selection , what do i select , see https://doc.qt.io/qt-5/qt.html#ItemSelectionMode-enum
+        # selection , what do i select , see https://doc.qt.io/qt-5/qt.html#ItemSelectionMode-enum
         self.setRubberBandSelectionMode(Qt.IntersectsItemBoundingRect)
 
-        #postition the view on resize
+        # postition the view on resize
         self.setResizeAnchor(GraphicsView.AnchorUnderMouse)
 
-        #position the view on transformation
+        # position the view on transformation
         self.setTransformationAnchor(GraphicsView.AnchorUnderMouse)
 
         # viewport see https://doc.qt.io/qt-5/qgraphicsview.html#ViewportUpdateMode-enum
@@ -54,19 +75,24 @@ class GraphicsView(QtWidgets.QGraphicsView):
 
         self.scale(0.8, 0.8)
 
-    def mouseMoveEvent(self,event):
-        #print('enterEvent inside view')
-        #self.viewport().setCursor(Qt.ClosedHandCursor)
+    def mouseMoveEvent(self, event):
+        # print('enterEvent inside view')
+        modifiers = QtWidgets.QApplication.keyboardModifiers()
+        if modifiers == QtCore.Qt.ShiftModifier:
+            self.viewport().setCursor(Qt.ClosedHandCursor)
+            self.setDragMode(GraphicsView.ScrollHandDrag)
+        else:
+            self.setDragMode(GraphicsView.RubberBandDrag)
+            self.viewport().setCursor(Qt.ArrowCursor)
         super(GraphicsView, self).mouseMoveEvent(event)
 
-    def mousePressEvent(self,event):
-        #print('mousePressEvent inside view')
-        #self.viewport().setCursor(Qt.ClosedHandCursor)
+    def mousePressEvent(self, event):
+        # print('mousePressEvent inside view')
+
         super(GraphicsView, self).mousePressEvent(event)
 
-    def mouseReleaseEvent(self,event):
-        #print('mouseReleaseEvent inside view')
-        #self.viewport().setCursor(Qt.ArrowCursor)
+    def mouseReleaseEvent(self, event):
+        # print("mouseReleaseEvent inside view")
         super(GraphicsView, self).mouseReleaseEvent(event)
 
     def wheelEvent(self, event):
@@ -76,7 +102,12 @@ class GraphicsView(QtWidgets.QGraphicsView):
     def scaleView(self, scaleFactor):
         """Transform function and zoom
         QrecF is the area visible"""
-        factor = self.transform().scale(scaleFactor, scaleFactor).mapRect(QRectF(0, 0, 50, 50)).width()
+        factor = (
+            self.transform()
+            .scale(scaleFactor, scaleFactor)
+            .mapRect(QRectF(0, 0, 50, 50))
+            .width()
+        )
         if factor < 0.07 or factor > 100:
             return
         self.scale(scaleFactor, scaleFactor)
